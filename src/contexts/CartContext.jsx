cb22226b9cc9d66bb42282213eb973004585ee37
MrSilent2001 <bandarigodadm.21@uom.lk>
2025-05-03 13:56:01 +0530

    contexts

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -12,7 +12,7 @@ const CartProvider = ({ children }) => {
 
 	useEffect(() => {
 		const total = cart.reduce((accumulator, currentItem) => {
-			return accumulator + currentItem.price;
+			return accumulator + currentItem.price * currentItem.amount;
 		}, 0);
 		setTotal(total);
 	}, [cart]);
@@ -29,7 +29,7 @@ const CartProvider = ({ children }) => {
 
 	// add to cart
 	const addToCart = (product, id) => {
-		const newItem = { ...product, amount: 2 };
+		const newItem = { ...product, amount: 1 };
 		// check if the item is already in the cart
 		const cartItem = cart.find((item) => {
 			return item.id === id;
@@ -37,7 +37,7 @@ const CartProvider = ({ children }) => {
 		if (cartItem) {
 			const newCart = [...cart].map((item) => {
 				if (item.id === id) {
-					return { ...item, amount: cartItem.amount };
+					return { ...item, amount: cartItem.amount + 1 };
 				} else return item;
 			});
 			setCart(newCart);
@@ -54,20 +54,36 @@ const CartProvider = ({ children }) => {
 		setCart(newCart);
 	};
 
-	// cleart cart
+	// clear cart
 	const clearCart = () => {
 		setCart([]);
 	};
 
 	// increase amount
 	const increaseAmount = (id) => {
-		const cartItem = cart.find((item) => item.id === id);
-		addToCart(cartItem, id);
+		const newCart = [...cart].map((item) => {
+			if (item.id === id) {
+				return { ...item, amount: item.amount + 1 };
+			} else return item;
+		});
+		setCart(newCart);
 	};
 
 	// decrease amount
 	const decreaseAmount = (id) => {
 		const cartItem = cart.find((item) => item.id === id);
+		if (cartItem) {
+			if (cartItem.amount === 1) {
+				removeFromCart(id);
+			} else {
+				const newCart = cart.map((item) => {
+					if (item.id === id) {
+						return { ...item, amount: item.amount - 1 };
+					} else return item;
+				});
+				setCart(newCart);
+			}
+		}
 	};
 
 	return (
--- src/contexts/SidebarContext.jsx
+++ src/contexts/SidebarContext.jsx
@@ -1,15 +1,82 @@
-import React, {useState, createContext} from 'react';
+import React, { useContext } from "react";
 
-export const SidebarContext = createContext();
+import { useNavigate } from "react-router-dom";
 
-const SidebarProvider = ({children}) => {
-  const [isOpen, setIsOpen] = useState(false);
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
 
-  const handleClose = () => {
-    setIsOpen(false)
-  }
+import CartItem from "../components/CartItem";
+import { SidebarContext } from "../contexts/SidebarContext.jsx";
+import { CartContext } from "../contexts/CartContext.jsx";
+import { CurrencyContext } from "../contexts/CurrencyContext.jsx";
 
-  return <SidebarContext.Provider value={{isOpen, setIsOpen, handleClose}}>{children}</SidebarContext.Provider>;
+const Sidebar = () => {
+	const navigate = useNavigate();
+
+	const { isOpen, handleClose } = useContext(SidebarContext);
+	const { cart, itemAmount, total, clearCart } = useContext(CartContext);
+	const { currencySymbol } = useContext(CurrencyContext);
+
+	const handleCheckout = () => {
+		if (cart.length === 0) {
+			alert(
+				"Your cart is empty. Please add items to your cart before checking out."
+			);
+			return;
+		}
+		handleClose();
+		navigate("/checkout");
+	};
+
+	return (
+		<div
+			className={`${
+				isOpen ? "right-0" : "-right-full"
+			} "w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] lg:w-[40vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]"`}
+		>
+			<div className="flex items-center justify-between py-6 border-b">
+				<div className="uppercase text-sm font-semibold">
+					Shopping Bag ({itemAmount})
+				</div>
+				<div
+					onClick={handleClose}
+					className="cursor-poniter w-8 h-8 flex justify-center items-center"
+				>
+					<IoMdArrowForward className="text-2xl" />
+				</div>
+			</div>
+			<div className="flex flex-col gap-y-2 h-[360px] md:h-[480px] lg:h-[420px] overflow-y-auto overflow-x-hidden border-b">
+				{cart.map((item) => {
+					return <CartItem item={item} key={item.id} />;
+				})}
+			</div>
+			<div className="flex flex-col gap-y-3  mt-4">
+				<div className="flex w-full justify-between items-center">
+					{/* total */}
+					<div className="font-semibold">
+						<span className="mr-2">Subtotal:</span>
+						{`${currencySymbol} ${parseFloat(total).toFixed(2)}`}
+					</div>
+					{/* clear cart icon */}
+					<div
+						onClick={clearCart}
+						className="clear-cart-btn cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl"
+					>
+						<FiTrash2 />
+					</div>
+				</div>
+				<button
+					type="button"
+					role="button"
+					aria-label="checkout"
+					onClick={handleCheckout}
+					className="bg-black flex p-3 justify-center items-center text-white w-full font-medium cursor-pointer"
+				>
+					Checkout
+				</button>
+			</div>
+		</div>
+	);
 };
 
-export default SidebarProvider;
+export default Sidebar;
